0c74321d4eff0d488821fe3e471fd978f0a32eb4
Mario Arana <marioknaus22@gmail.com>
2025-03-06 15:45:33 +0100

    Simplify ex1-9

--- chapter_01/remove_multiblanks_ex1-9.c
+++ chapter_01/remove_multiblanks_ex1-9.c
@@ -3,16 +3,10 @@
 int main() {
     int ch, prev_char = 0;
 
-    printf("Okay, you can start typing stuff, I will repeat without unnecessary blanks.\n");
+    printf("You can start typing stuff, I will repeat without unnecessary blanks.\n");
 
     while ((ch = getchar()) != EOF) {
-        if (ch == ' ') {
-            if (prev_char != ' ') {
-                putchar(ch);
-            } else {
-                // Do nothing
-            }
-        } else {
+        if (ch != ' ' || prev_char != ' ') {
             putchar(ch);
         }
         
